333a444dd3e53997017818f3127ebf254453fd34
Lee Benson <lee.benson@imperial.ac.uk>
2025-02-22 15:19:41 +0000

    See REST_auth_login for how to send request to a protected resource via REST API

--- tests/REST_auth_login.py
+++ tests/REST_auth_login.py
@@ -7,8 +7,23 @@ r = requests.post(f"{BASEURL}/login",
                           "password": "secret123"},
                   verify = False)
 
-csrf_token = r.cookies["csrftoken"]
+cookies = r.cookies
+csrftoken = cookies["csrftoken"]
+session = cookies["session"]
+                    
+#breakpoint()
 
-r = requests.post(f"{BASEURL}/logout",
-                  headers = {"X-CSRF-Token": csrf_token},
+
+r = requests.post(f"{BASEURL}/records",
+                  cookies = dict(session=session, csrftoken=csrftoken),
+                  headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
                   verify = False)
+
+#r = requests.get(f"{BASEURL}/records",
+#                 headers = {"X-CSRF-Token": csrf_token},
+#                 verify = False)
+
+
+#r = requests.post(f"{BASEURL}/logout",
+#                  headers = {"X-CSRF-Token": csrf_token},
+#                  verify = False)
