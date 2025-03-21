c1ca1c8fa46e02a733270d9d6e03ac883a6a68f9
Lee Benson <lee.benson@imperial.ac.uk>
2025-03-21 19:35:03 +0000

    Record-Community domain metadata jsonschema validation 422 REST API flow tests written and passing

--- tests/REST_access_token_CRUD_community.py
+++ tests/REST_access_token_CRUD_community.py
@@ -4,39 +4,60 @@ import requests
 from pprint import pprint
 
 BASEURL = "https://127.0.0.1:5000/api"
+TOKEN = os.environ.get("TOKEN", None)
+COMMUNITY_SLUG = "tea"
 
-ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", None)
-if not ACCESS_TOKEN:
+if not TOKEN:
     sys.exit("ACCESS_TOKEN not set")
-                    
+
+
+#0. Get community ID
+r = requests.get(f"{BASEURL}/communities",
+                 params = {"slug": COMMUNITY_SLUG},
+                 verify = False)
+
+
+assert r.status_code == 200 #OK
+assert r.json()["hits"]["total"] == 1
+community_id = r.json()["hits"]["hits"][0]["id"]
+
+
+
 #1. Create new record
 r = requests.post(f"{BASEURL}/records",
-                  params = {"access_token": ACCESS_TOKEN},
+                  params = {"access_token": TOKEN},
                   verify = False)
 
 assert r.status_code == 201 #Created
 record = r.json()
 record_id = record["id"]
 
+#2. Add to community
+r = requests.put(f"{BASEURL}/records/{record_id}/draft/review",
+                 params = {"access_token": TOKEN},
+                 json = {"receiver": {"community": community_id}, "type": "community-submission"},
+                 verify = False)
+
+
+
 #3. Add some invalid domain metadata to record 
 record["metadata"] = {"domain_metadata": {"entry_type": {"longitude": 22.0,
                                                          "laaaatitude": 22.0}}}
 r = requests.put(f"{BASEURL}/records/{record_id}/draft",
-                 params = {"access_token": ACCESS_TOKEN},
+                 params = {"access_token": TOKEN},
                  verify = False,
                  json = record)
 
-assert r.status_code == 500 #Internal Server Error
-#assert "jsonschema.exceptions.ValidationError: {\'longitude\': 22.0, \'laaaatitude\': 22.0} is not valid under any of the given schemas" in r.text
+assert r.status_code == 422 # Is this an appropriate status_code?
+assert "{\'longitude\': 22.0, \'laaaatitude\': 22.0} is not valid under any of the given schemas" in r.text
 
 
-'''
+
 #4. Add required metadata & some valid domain metadata to record
 record["metadata"] = {"domain_metadata": {"entry_type": {"longitude": 22.0,
                                                          "latitude": 22.0}}}
 r = requests.put(f"{BASEURL}/records/{record_id}/draft",
-                 cookies = dict(session=session, csrftoken=csrftoken),
-                 headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                 params = {"access_token": TOKEN},
                  verify = False,
                  json = record)
 
@@ -70,17 +91,16 @@ record["metadata"]["title"] = "Test record!"
 record["metadata"]["publication_date"] = "1970/2016-06"
 
 r = requests.put(f"{BASEURL}/records/{record_id}/draft",
-                 cookies = dict(session=session, csrftoken=csrftoken),
-                 headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                 params = {"access_token": TOKEN},
                  verify = False,
                  json = record)
+
 
 #5. Start file upload
 data = [{"key": "spongebob.png"}, {"key": "ideal-hash-trees.pdf"}]
 
 r = requests.post(f"{BASEURL}/records/{record_id}/draft/files",
-                  cookies = dict(session=session, csrftoken=csrftoken),
-                  headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                  params = {"access_token": TOKEN},
                   verify = False,
                   json = data)
 
@@ -90,8 +110,7 @@ assert r.status_code == 201 #Created
 with open("tests/test_files/spongebob.png", "rb") as f:
     filename = "spongebob.png"
     r = requests.put(f"{BASEURL}/records/{record_id}/draft/files/{filename}/content",
-                     cookies = dict(session=session, csrftoken=csrftoken),
-                     headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                     params = {"access_token": TOKEN},
                      verify = False,
                      data = f)
 
@@ -100,8 +119,7 @@ assert r.status_code == 200 #OK
 with open("tests/test_files/idealhashtrees.pdf", "rb") as f:
     filename = "ideal-hash-trees.pdf"
     r = requests.put(f"{BASEURL}/records/{record_id}/draft/files/{filename}/content",
-                     cookies = dict(session=session, csrftoken=csrftoken),
-                     headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                     params = {"access_token": TOKEN},
                      verify = False,
                      data = f)
 
@@ -110,16 +128,14 @@ assert r.status_code == 200 #OK
 #7. Complete draft file upload
 filename = "spongebob.png"
 r = requests.post(f"{BASEURL}/records/{record_id}/draft/files/{filename}/commit",
-                  cookies = dict(session=session, csrftoken=csrftoken),
-                  headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                  params = {"access_token": TOKEN},
                   verify = False)
 
 assert r.status_code == 200 #OK
 
 filename = "ideal-hash-trees.pdf"
 r = requests.post(f"{BASEURL}/records/{record_id}/draft/files/{filename}/commit",
-                  cookies = dict(session=session, csrftoken=csrftoken),
-                  headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                  params = {"access_token": TOKEN},
                   verify = False)
 
 assert r.status_code == 200 #OK
@@ -127,21 +143,17 @@ assert r.status_code == 200 #OK
 #8. Delete a file
 filename = "ideal-hash-trees.pdf"
 r = requests.delete(f"{BASEURL}/records/{record_id}/draft/files/{filename}",
-                    cookies = dict(session=session, csrftoken=csrftoken),
-                    headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
+                    params = {"access_token": TOKEN},
                     verify = False)
 
 assert r.status_code == 204 #No Content
 
-#9.  Publish record
-#  -- first a
+#9. Request review
+r = requests.post(f"{BASEURL}/records/{record_id}/draft/actions/submit-review",
+                  params = {"access_token": TOKEN},
+                  verify = False,
+                  json = {"payload": {"content": "Thanking you kindly", "format": "html"}})
 
-r = requests.post(f"{BASEURL}/records/{record_id}/draft/actions/publish",
-                  cookies = dict(session=session, csrftoken=csrftoken),
-                  headers = {"X-CSRFToken": csrftoken, "Referer": "https://localhost"},
-                  verify = False)
 
-assert r.status_code == 202 #Accepted
-'''
 
 
